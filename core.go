e58c9f85c65a4db028cb7a287bbbd06798719b74
Hernan Reyes <hernan_rm@outlook.es>
2023-10-27 09:04:38 -0600

    Feat: adds core logic to set and get dependencies

---
+++ core.go
@@ -0,0 +1,53 @@
+package dependor
+
+import (
+	"errors"
+)
+
+var ErrNotFound = errors.New("dependor: dependency not found")
+var ErrInvalidType = errors.New("dependor: invalid dependency type")
+
+func init() {
+	container = make(map[string]dependencyMetadata)
+}
+
+type dependencyMetadata struct {
+	value     any
+	dependsOn []string
+}
+
+var container map[string]dependencyMetadata
+
+// Set sets a dependency with a name and defines its dependencies
+func Set[T any](name string, value T, dependsOn []string) {
+	if container == nil {
+		container = make(map[string]dependencyMetadata)
+	}
+
+	container[name] = dependencyMetadata{
+		value:     value,
+		dependsOn: dependsOn,
+	}
+}
+
+// Get gets the dependency omitting any error
+func Get[T any](name string) T {
+	value, _ := GetWithErr[T](name)
+	return value
+}
+
+// GetWithErr gets a dependency and a posible error
+func GetWithErr[T any](name string) (T, error) {
+	var value T
+	dependency, ok := container[name]
+	if !ok {
+		return value, ErrNotFound
+	}
+
+	value, ok = dependency.value.(T)
+	if !ok {
+		return value, ErrInvalidType
+	}
+
+	return value, nil
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/techforge-lat/dependor
+
+go 1.21.3
